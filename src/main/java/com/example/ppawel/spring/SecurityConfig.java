5520b9bd55489781a10bd21c12943b70c7ce2af6
Paweł Paprota <pawel@jcore.pl>
2016-02-04 23:56:57 +0100

    Fixed logout configuration, added logout link to the home page.

--- src/main/java/com/example/ppawel/spring/SecurityConfig.java
+++ src/main/java/com/example/ppawel/spring/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.ppawel.service.UserService;
 
@@ -25,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/register").permitAll().antMatchers("/", "/home").fullyAuthenticated()
 				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-				.permitAll();
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
 	}
 
 	@Autowired
